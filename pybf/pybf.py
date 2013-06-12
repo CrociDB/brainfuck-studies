64c3113381b25f1fb9f402150b140e32c071b41a
Bruno Croci <crocidb@gmail.com>
2013-06-12 01:38:36 -0300

    changed tabs to spaces

--- pybf/pybf.py
+++ pybf/pybf.py
@@ -3,110 +3,110 @@ import collections
 
 class Brainfuck:
 
-	def __init__(self):
-		self.cells = [0 for i in range(9999)]
-		self.pointer = 0
-		self.sp = 0
-		self.has_error = False
+    def __init__(self):
+        self.cells = [0 for i in range(9999)]
+        self.pointer = 0
+        self.sp = 0
+        self.has_error = False
 
-	def load(self, filename):
-		try:
-			self.source = open(filename, "r")
-		except IOError:
-			print "Error loading source"
-
-
-		self.source = self.source.read()
-		self.tokens = self.tokenize()
-
-		self.interpret(self.tokens)
-
-	def tokenize(self):
-		tokens = []
-
-		while self.sp < len(self.source):
-			c = self.source[self.sp]
-
-			if c in [',', '.', '>', '<', '+', '-']:
-				tokens.append(c)
-			elif c == '[':
-				self.sp += 1
-				sub_token = self.tokenize()
-				tokens.append(sub_token)
-			elif c == ']':
-				break
-
-			self.sp += 1
-
-		return tokens
+    def load(self, filename):
+        try:
+            self.source = open(filename, "r")
+        except IOError:
+            print "Error loading source"
 
 
-	def interpret(self, tokens):
-		sp = 0
+        self.source = self.source.read()
+        self.tokens = self.tokenize()
 
-		while sp < len(tokens):
-			if self.has_error:
-				return
+        self.interpret(self.tokens)
 
-			c = tokens[sp]
+    def tokenize(self):
+        tokens = []
 
-			if   c == ',':
-				self.input()
-			elif c == '.':
-				self.output()
-			elif c == '>':
-				self.next_cell()
-			elif c == '<':
-				self.previous_cell()
-			elif c == '+':
-				self.increase_cell()
-			elif c == '-':
-				self.decrease_cell()
-			elif isinstance(c, collections.Iterable):
-				while self.current_cell_not_zero(): self.interpret(c)
-				
-			sp += 1
+        while self.sp < len(self.source):
+            c = self.source[self.sp]
+
+            if c in [',', '.', '>', '<', '+', '-']:
+                tokens.append(c)
+            elif c == '[':
+                self.sp += 1
+                sub_token = self.tokenize()
+                tokens.append(sub_token)
+            elif c == ']':
+                break
+
+            self.sp += 1
+
+        return tokens
 
 
-	def current_cell_not_zero(self):
-		return self.cells[self.pointer] != 0
+    def interpret(self, tokens):
+        sp = 0
 
-	def input(self):
-		self.cells[self.pointer] = ord(raw_input()[0])
+        while sp < len(tokens):
+            if self.has_error:
+                return
 
-	def output(self):
-		sys.stdout.write(chr(self.cells[self.pointer]))
+            c = tokens[sp]
 
-	def next_cell(self):
-		if self.pointer == len(self.cells): 
-			self.error("No more cells ahead")
-			return;
-
-		self.pointer += 1
-
-
-	def previous_cell(self):
-		if self.pointer == 0: 
-			self.error("This is the first cell already")
-			return;
-
-		self.pointer -= 1
-
-	def increase_cell(self):
-		self.cells[self.pointer] += 1
-
-	def decrease_cell(self):
-		self.cells[self.pointer] -= 1
+            if   c == ',':
+                self.input()
+            elif c == '.':
+                self.output()
+            elif c == '>':
+                self.next_cell()
+            elif c == '<':
+                self.previous_cell()
+            elif c == '+':
+                self.increase_cell()
+            elif c == '-':
+                self.decrease_cell()
+            elif isinstance(c, collections.Iterable):
+                while self.current_cell_not_zero(): self.interpret(c)
+                
+            sp += 1
 
 
-	def error(self, message):
-		print("### ERROR: %s" % message)
-		self.has_error = True
-		
+    def current_cell_not_zero(self):
+        return self.cells[self.pointer] != 0
+
+    def input(self):
+        self.cells[self.pointer] = ord(raw_input()[0])
+
+    def output(self):
+        sys.stdout.write(chr(self.cells[self.pointer]))
+
+    def next_cell(self):
+        if self.pointer == len(self.cells): 
+            self.error("No more cells ahead")
+            return;
+
+        self.pointer += 1
+
+
+    def previous_cell(self):
+        if self.pointer == 0: 
+            self.error("This is the first cell already")
+            return;
+
+        self.pointer -= 1
+
+    def increase_cell(self):
+        self.cells[self.pointer] += 1
+
+    def decrease_cell(self):
+        self.cells[self.pointer] -= 1
+
+
+    def error(self, message):
+        print("### ERROR: %s" % message)
+        self.has_error = True
+        
 
 if __name__ == "__main__":
-	if len(sys.argv) == 1:
-		print("\npybf.py -- no file specified. how to use: \n\t$ python pybf.py sourcefile\n")
-	else:
-		bf = Brainfuck()
-		bf.load(sys.argv[1])
+    if len(sys.argv) == 1:
+        print("\npybf.py -- no file specified. how to use: \n\t$ python pybf.py sourcefile\n")
+    else:
+        bf = Brainfuck()
+        bf.load(sys.argv[1])
